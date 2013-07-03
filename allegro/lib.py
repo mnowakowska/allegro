ad46b71e88382636f1e4a7ad84ddeb17c0dabb6b
Malwina Nowakowska <malwina.nowakowska@stxnext.pl>
2013-07-03 07:24:21 +0200

    ladniejsze

--- allegro/lib.py
+++ allegro/lib.py
@@ -12,30 +12,33 @@ def downolad_data(product):
         browser.open("http://allegro.pl")
     except (mechanize.HTTPError, mechanize.URLError):
         raise AllegroError('No connection with allegro.pl')
-    else:
-        browser.select_form(nr=0)
-        browser.form['string'] = product
-        browser.submit()
-        try:
-            browser.select_form(name='display-form')
-        except mechanize._mechanize.FormNotFoundError:
-            raise AllegroError('Allegro takes exactly 1 argument!')
-        else:
-            browser.find_control('offerTypeBuyNow').items[0].selected = True
-            browser.find_control('standard_allegro').items[0].selected = True
-            browser.find_control('startingTime').items[7].selected = False
-            browser.find_control('shippingTime').items[0].selected = False
-            browser.submit()
-            html = browser.response().read()
-            soup = BeautifulSoup(html)
-            offers = soup.find('section', class_='offers')
-            if offers is None:
-                raise AllegroError('No result!!')
-            else:
-                url = 'http://allegro.pl' + offers.find('a').get('href')
-                price = offers.div.find('span', class_='buy-now dist')
-                price = price.contents[2]
-                price = ''.join(price.split())
-                price = price.replace(',', '.')
-                price = float(price)
-                return url, price
+
+    browser.select_form(nr=0)
+    browser.form['string'] = product
+    browser.submit()
+
+    try:
+        browser.select_form(name='display-form')
+    except mechanize._mechanize.FormNotFoundError:
+        raise AllegroError('Allegro takes exactly 1 argument!')
+
+    browser.find_control('offerTypeBuyNow').items[0].selected = True
+    browser.find_control('standard_allegro').items[0].selected = True
+    browser.find_control('startingTime').items[7].selected = False
+    browser.find_control('shippingTime').items[0].selected = False
+    browser.submit()
+    html = browser.response().read()
+    soup = BeautifulSoup(html)
+    offers = soup.find('section', class_='offers')
+
+    if not offers:
+        raise AllegroError('No result!!')
+
+    url = 'http://allegro.pl' + offers.find('a').get('href')
+
+    price = offers.div.find('span', class_='buy-now dist')
+    price = price.contents[2]
+    price = ''.join(price.split())
+    price = price.replace(',', '.')
+    price = float(price)
+    return url, price
